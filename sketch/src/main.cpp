5a8fc0b8d24b05d70295febaa7c7b590e0b7310b
nicolovinci <12999303@itisrossi.vi.it>
2026-05-05 09:10:24 +0200

    Updated platformio.ini

--- sketch/src/main.cpp
+++ sketch/src/main.cpp
@@ -1,18 +1,10 @@
 #include <Arduino.h>
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(115200);
-    delay(1000);
-    Serial.println("SentinelBoard Arduino UNO avviato");
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
-
     Serial.println("$TEL;TIME=1000;TEMP=24.5;HUM=51.0;DIST=38.0;MOTION=0;LIGHT=420;STATUS=OK");
     delay(1000);
 }
