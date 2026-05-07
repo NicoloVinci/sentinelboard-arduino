b7b35186d7e7d63dee8d407ff4e57fd4f10cef15
nicolovinci <12999303@itisrossi.vi.it>
2026-05-07 11:16:02 +0200

    Need to check if it works

--- sketch/src/main.cpp
+++ sketch/src/main.cpp
@@ -1,10 +1,49 @@
 #include <Arduino.h>
 
+bool systemStarted = false;
+String inputLine = "";
+unsigned long lastSampleTime = 0;
+unsigned long timeCounter = 0;
+
 void setup() {
     Serial.begin(115200);
 }
 
+void handleCommand(const String& cmd) {
+    if (cmd == "START") {
+        systemStarted = true;
+        timeCounter = 0;
+        Serial.println("ACK_START");
+    } else if (cmd == "STOP") {
+        systemStarted = false;
+        Serial.println("ACK_STOP");
+    }
+}
+
 void loop() {
-    Serial.println("$TEL;TIME=1000;TEMP=24.5;HUM=51.0;DIST=38.0;MOTION=0;LIGHT=420;STATUS=OK");
-    delay(1000);
+    while (Serial.available() > 0) {
+        char c = (char)Serial.read();
+
+        if (c == '\n') {
+            inputLine.trim();
+            if (!inputLine.length() > 0)
+                handleCommand(inputLine);
+            inputLine = "";
+        } else {
+            inputLine += c;
+        }
+    }
+
+    if (systemStarted) {
+        unsigned long now = millis();
+        if (now - lastSampleTime >= 1000) {
+            lastSampleTime = now;
+            timeCounter += 1000;
+
+            Serial.print("$TEL;TIME=");
+            Serial.print(timeCounter);
+            Serial.print(";TEMP=24.5;HUM=51.0;DIST=38.0;MOTION=0;LIGHT=420;STATUS=OK");
+            Serial.println();
+        }
+    }
 }
